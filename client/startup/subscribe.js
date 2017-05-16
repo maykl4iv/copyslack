637c47df45690be2a18a47cd787534ba75cdf036
maykl4iv <mishaiv1995@gmail.com>
2017-05-16 15:50:34 +0300

    thirdCommit

---
+++ client/startup/subscribe.js
@@ -0,0 +1,10 @@
+Meteor.subscribe('channels');
+
+Template.messages.onCreated( function(){
+	var self = this;
+		self.autorun( function(){
+			self.subscribe( 'messages', Session.get('channel') );
+		});
+});
+
+
---
+++ server/seeder.js
@@ -0,0 +1,37 @@
+Meteor.startup(function() {
+
+  
+
+  Meteor.users.remove({});
+  Accounts.createUser({
+    username: "Misha",
+    email: "mishaiv1995@gmail.com",
+    password: "mishamisha"
+  });
+
+  Factory.define('message', Messages, {
+    text: function() {
+      return Fake.sentence();
+    },
+    user: Meteor.users.findOne()._id,
+    timestamp: Date.now(),
+    channel: 'general'
+  });
+
+  // Add this if you want to remove all messages before seeding
+  Messages.remove({});
+
+  if (Messages.find({}).count() === 0) {
+    _(5).times(function(n) {
+      Factory.create('message');
+    });
+  }
+});
+
+Channels.remove({});
+Channels.insert({
+  name: 'general'
+});
+Channels.insert({
+  name: 'random'
+});
